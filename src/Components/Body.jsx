4e8c10524e1aea63a5972cdd52d34ba875b552e1
Ashu Singh <ashusingh2201@gmail.com>
2025-12-15 18:40:03 +0000

    nav

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,7 +3,7 @@ import  Container  from "./Container"
 const Body = () => {
   return (
      <div className="min-h-screen flex items-start justify-start">
-      <Container className="min-h-screen">
+      <Container className="min-h-[200vh] p-4 md:p-10">
       <h1 className="text-2xl md:text-4xl font-bold tracking-tight text-primary">Hello there</h1>
       <p className="text-secondary   text-sm  md:text-base">I'm a software who specializes in frontend part of the system</p>
       </Container>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react' 
+import {motion, useMotionValueEvent, useScroll} from 'motion/react'
+
 
 const Navbar = () => {
 
@@ -19,11 +21,46 @@ const Navbar = () => {
       title:"Blog",
       href:'/blog'
     }
-  ]
+  ];
+
+  const [hovered,setHovered]=useState();
+  const {scrollY}=useScroll();
+
+  const[shadow,setShadow]=useState("")
+
+  useMotionValueEvent(scrollY,"change",(latest)=>{
+    if(latest>20){
+      setShadow(true)
+    }else{
+      setShadow(false)
+    }
+  })
+
   return (
-    <div className='flex items-center justify-between'>
-      <img src={} alt="profile.png"></img>
-    </div>
+    <>
+    <motion.nav
+    style={{
+      boxShadow:shadow? 'var(--shadow-acer)':"none"
+    }}
+     className='flex fixed top-0 inset-x-0 max-w-4xlitems-center justify-between p-2'>
+      <img className='w-10 h-10 rounded-full' src="/profile.jpg" height="100" width="100" alt="profile"></img>
+
+      <div className='flex items-center '>
+        {navItems.map((item,index)=>(
+          <a href={item.href} key={index} className='text-sm relative px-2 py-1'
+          onMouseEnter={()=>setHovered(index)}
+          onMouseLeave={()=>setHovered()}>
+            { hovered ===index &&
+              (<motion.span layoutId='hovered-span' className='h-full w-full absolute inset-0 rounded-md bg-neutral-100 dark:bg-neutral-800'>
+              </motion.span>)
+            }
+            <span className='z-10 relative'> {item.title} </span>
+          </a>
+        ))}
+      </div>
+      
+    </motion.nav>
+    </>
   )
 }
 
